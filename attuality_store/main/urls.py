37c8cd090f62f11093c99c4886a299d1ebf87088
Hassan Aziz <hassanaziz0012@gmail.com>
2022-05-10 19:09:24 +0500

    Added all footer items

--- attuality_store/main/urls.py
+++ attuality_store/main/urls.py
@@ -4,4 +4,12 @@ from main import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
     path('attuality-store', views.AttualityStoreView.as_view(), name="attuality-store"),
+    path('privacy', views.PrivacyView.as_view(), name="privacy"),
+    path('domande_frequenti', views.DomandeFrequentiView.as_view(), name="domande-frequenti"),
+    path('contattaci', views.ContattaciView.as_view(), name="contattaci"),
+    path('spedizione_resi_gratuiti', views.SpedizioneResiGratuitiView.as_view(), name="spedizione-resi-gratuiti"),
+    path('pagamento', views.PagamentoView.as_view(), name="pagamento"),
+    path('spedizione', views.SpedizioneView.as_view(), name="spedizione"),
+    path('resi_cambi', views.ResiCambiView.as_view(), name="resi-cambi"),
+    path('servizi', views.ServiziView.as_view(), name="servizi"),
 ]
--- attuality_store/main/views.py
+++ attuality_store/main/views.py
@@ -11,3 +11,44 @@ class HomeView(View):
 class AttualityStoreView(View):
     def get(self, request):
         return render(request, 'footer/attuality_store.html')
+
+
+class PrivacyView(View):
+    def get(self, request):
+        return render(request, 'footer/privacy.html')
+
+
+class DomandeFrequentiView(View):
+    def get(self, request):
+        return render(request, 'footer/domande_frequenti.html')
+
+
+class ContattaciView(View):
+    def get(self, request):
+        return render(request, 'footer/contattaci.html')
+
+
+class SpedizioneResiGratuitiView(View):
+    def get(self, request):
+        return render(request, 'footer/spedizione_e_resi_gratuiti.html')
+
+
+class PagamentoView(View):
+    def get(self, request):
+        return render(request, 'footer/pagamento.html')
+
+
+class SpedizioneView(View):
+    def get(self, request):
+        return render(request, 'footer/spedizione.html')
+
+    
+class ResiCambiView(View):
+    def get(self, request):
+        return render(request, 'footer/resi_cambi.html')
+
+
+class ServiziView(View):
+    def get(self, request):
+        return render(request, 'footer/servizi_online_esclusivi.html')
+
