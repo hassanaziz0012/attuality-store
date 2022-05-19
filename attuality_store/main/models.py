85244e8a27ec001fc25ef27cd5648c02475e03bd
Hassan Aziz <hassanaziz0012@gmail.com>
2022-05-19 21:38:02 +0500

    Add brand field

--- attuality_store/main/models.py
+++ attuality_store/main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=500, unique=True)
 
+    brand = models.CharField(max_length=100, null=True, blank=False)
     price = models.FloatField(default=0)
     discounted_price = models.FloatField(default=0)
     sizes = models.JSONField(null=True, blank=True)
